736a6a530e444c020b681acc3dde01dbb3221d44
toufikcse <toufikur.rahman.toufik@gmail.com>
2022-02-25 01:56:55 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+const cors = require('cors');
+require('dotenv').config()
+
+const app = express()
+const port = 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mrzdw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run () {
+    try {
+        await client.connect();
+
+        const database = client.db("carMechanicsDB");
+        const servicesCollection = database.collection("servicesCollection");
+
+        // GET API
+        // Find Multiple Documents
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        // GET API
+        // Find a Document
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await servicesCollection.findOne(query);
+            res.send(result);
+        });
+
+        // POST API
+        // Insert a Document
+        app.post('/services', async (req, res) => {
+            const service = req.body;
+            const result = await servicesCollection.insertOne(service);
+            res.json(result);
+        });
+
+        // DELETE API
+        // Delete a Document
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await servicesCollection.deleteOne(query);
+            res.json(result);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Welcome Car Mechanics Server');
+});
+
+app.listen(port, () => {
+  console.log('app listening on port', port);
+});
